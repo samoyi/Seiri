c25f371fd887de4cabc6573f03eb94de4c7dea47
samoyi <leening529@163.com>
2018-01-23 08:56:25 +0800

    正在测试异步效果

--- BackEnd/test.js
+++ BackEnd/test.js
@@ -1,26 +1,42 @@
 const Koa = require('koa');
+const compose = require('koa-compose');
 const app = new Koa();
 
-const one = async (ctx, next) => {
-  console.log(1);
-  await next();
-  console.log(6);
-}
 
-const two = async (ctx, next) => {
-  console.log(2);
-  await next();
-  console.log(5);
-}
 
-const three = async (ctx, next) => {
-  console.log(3);
-  await next();
-  console.log(4);
-}
+const fs = require('fs');
+const {promisify} = require('util');
+const readdir = promisify(fs.readdir);
+const readFile = promisify(fs.readFile);
 
-app.use(one);
-app.use(two);
-app.use(three);
+app.use(async function (ctx, next) {
+
+
+    console.log('1 ' + Date.now());
+  const paths = await readdir('Doc');
+  console.log('2 ' + Date.now());
+  const files = await Promise.all(paths.map(path => readFile(`Doc/${path}`, 'utf8')));
+console.log('3 ' + Date.now());
+await next();
+  ctx.type = 'markdown';
+  ctx.body = Date.now() + files.join('<br /><br />');
+});
+
+
+function logger(format) {
+  format = format || ':method ":url"';
+
+  return async function (ctx, next) {
+    const str = format
+      .replace(':method', ctx.method)
+      .replace(':url', ctx.url);
+
+    console.log(Date.now() + ' ' + str);
+
+    await next();
+  };
+}
+app.use(logger());
+
 
 app.listen(3000);
